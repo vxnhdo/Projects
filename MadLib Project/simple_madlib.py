0dc7d2d73f498e0da09569626f88a231a50a0f85
vxnhdo <vinniedo1680@gmail.com>
2025-06-28 14:53:58 +0000

    MadLib projects.

---
+++ MadLib Project/simple_madlib.py
@@ -0,0 +1,13 @@
+# Blanks inside of paragraph that users will enter into, while using string concatenation (how to put strings together)
+
+# Example:
+# youtuber = "Notvinh"
+# print(f"Subscribe to {youtuber}!")
+
+adj = input("Adjective (Characteristic): ")
+verb1 = input("Verb (Action): " )
+verb2 = input("Verb (Action): " )
+famous_person = input("Name a celebrity: ")
+
+madlib = print(f"Computer programming is so {adj}! It makes me excited all the time because I love to {verb1}. Stayed hydrated and {verb2} like you are {famous_person}.")
+
